f077ab748200a661614d3fa83f218f4f61bc01f0
Nastya <nastya.syasyafi@gmail.com>
2024-11-22 18:00:33 +0200

    Initial commit

---
+++ src/utils/readContacts.js
@@ -0,0 +1,9 @@
+import { readFile } from "node:fs/promises";
+
+import { PATH_DB } from "../constants/contacts.js";
+
+export const readContacts = async () => {
+  const data = await readFile(PATH_DB, "utf-8");
+  console.log(data);
+  return JSON.parse(data);
+};
